57b048a3947cf4d1a696c3cb1bf6c27d4f45edab
kfparri <kfparri@nppd.com>
2020-04-02 10:09:39 -0500

    I've updated the deploy script to be a little more reliable and cleaner

--- createNewDocument.bat
+++ createNewDocument.bat
@@ -1,21 +1,26 @@
+::@Echo Off
 :: %1 is the first parameter.  In this case, the parameter is the destination folder and is required
 
+:: Usage createnewDocument <ProjectName> <DestinationPAth>
 IF %1.==. GOTO MissingParam
+IF %2.==. GOTO MissingParam
 
 :: First, copy the document files
-XCOPY software-specification-template %1\%1-specification\ /E 
-XCOPY .gitignore %1\.gitignore
+XCOPY software-specification-template %2\%1-specification\ /E 
+XCOPY .gitignore %2\
 
-pusd %1
+pushd %2
 
 git init
 
 popd
 
-Echo Done!  Your new documentation project can be found here %1\%1-specification\
+Echo "Done!  Your new documentation project can be found here %1\%1-specification\"
+
+GOTO End
 
 :MissingParam
-    Echo Usage: createNewDocument <Destination Path>
+    Echo Usage: createNewDocument ProjectNameDestinationPath
     GOTO End
 
 :End
